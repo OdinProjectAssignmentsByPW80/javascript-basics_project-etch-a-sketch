71f3aa816ef15593fd03bfe595ceb92e05d0d963
Paul Williams <advanced_solution_delivery@outlook.com>
2025-06-08 15:27:17 +0100

    feat: add change resolution
    Changes to be committed:
    	modified:   index.html
    	modified:   script.js
    	modified:   style.css

--- script.js
+++ script.js
@@ -18,6 +18,17 @@ function createPixels(xRes, yRes) {
     pixel.style.flexBasis = `${basis}px`;
     easScreen.appendChild(pixel);
   }
+  addPixelEventListener();
+}
+
+/**
+ * Clears all the child elements from #screen
+ */
+function clearPixels() {
+  const easScreen = document.querySelector("#screen");
+  while (easScreen.firstChild) {
+    easScreen.removeChild(easScreen.lastChild);
+  }
 }
 
 /**
@@ -30,14 +41,42 @@ function activatePixel(event) {
   event.target.setAttributeNode(att);
 }
 
-let xRes = 16;
-let yRes = 16;
-
-createPixels(xRes, yRes);
-
-const pixels = document.getElementsByClassName("pixel");
-for (let pixel of pixels) {
-  pixel.addEventListener("mouseover", (event) => {
-    activatePixel(event);
-  });
+/**
+ * Checks the value of #resolution number input. Prompts if out of range
+ * otherwise clears #screen and re-populates the area with new .pixel elements
+ */
+function setResolution() {
+  const res = document.querySelector("#resolution").value;
+  if (res < 1) prompt("Resolution input value too low.");
+  else if (res > 100) prompt("Resolution input value too high.");
+  else {
+    clearPixels();
+    createPixels(res, res);
+  }
 }
+
+/**
+ * Adds mouseover an event listener that calls activatePixel to each .pixel
+ * element
+ */
+function addPixelEventListener() {
+  const pixels = document.getElementsByClassName("pixel");
+  for (let pixel of pixels) {
+    pixel.addEventListener("mouseover", (event) => {
+      activatePixel(event);
+    });
+  }
+}
+
+// Starting #screen resolution
+createPixels(16, 16);
+
+// event listener for the resolution submit button
+const resolutionSubmit = document.querySelector("#resolutionSubmit");
+resolutionSubmit.addEventListener("click", () => {
+  setResolution();
+});
+
+// todo: reset button
+// todo: rainbow mode
+// todo: fade out
