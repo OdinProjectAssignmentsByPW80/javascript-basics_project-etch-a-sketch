fa4cb7f3799f4695b5253c3b5837a834f544c550
Paul Williams <advanced_solution_delivery@outlook.com>
2025-06-09 09:45:22 +0100

    refactor: change html ids, match in css and js, organise script
    Changes to be committed:
    	modified:   index.html
    	modified:   script.js
    	modified:   style.css

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 "use strict";
 
+// set initial #screen resolution on load
+createPixels(16, 16);
+
 /**
  * Creates xRes * yRes div elements with the class="pixel" and appends them to
  * the #screen element. Sets flex-basis for each .pixel to fit in parent.
  * @param {int} xRes The horizontal resolution for the .pixels.
- * @param {*} yRes The vertical resolution for the .pixels.
+ * @param {int} yRes The vertical resolution for the .pixels.
  */
 function createPixels(xRes, yRes) {
   const easScreen = document.querySelector("#screen");
   const resolution = xRes * yRes;
   const basis = easScreen.offsetWidth / xRes;
+  clearPixels();
   for (let i = 0; i < resolution; i++) {
     const pixel = document.createElement("div");
     const att = document.createAttribute("class");
@@ -32,30 +36,6 @@ function clearPixels() {
 }
 
 /**
- * Adds the class active to the event's target element
- * @param {MouseEvent<"mouseover">} event
- */
-function activatePixel(event) {
-  const att = document.createAttribute("class");
-  att.value = "pixel active";
-  event.target.setAttributeNode(att);
-}
-
-/**
- * Checks the value of #resolution number input. Prompts if out of range
- * otherwise clears #screen and re-populates the area with new .pixel elements
- */
-function setResolution() {
-  const res = document.querySelector("#resolution").value;
-  if (res < 1) prompt("Resolution input value too low.");
-  else if (res > 100) prompt("Resolution input value too high.");
-  else {
-    clearPixels();
-    createPixels(res, res);
-  }
-}
-
-/**
  * Adds mouseover an event listener that calls activatePixel to each .pixel
  * element
  */
@@ -68,14 +48,34 @@ function addPixelEventListener() {
   }
 }
 
-// Starting #screen resolution
-createPixels(16, 16);
+/**
+ * Adds the class active to the event's target element
+ * @param {MouseEvent<"mouseover">} event
+ */
+function activatePixel(event) {
+  const att = document.createAttribute("class");
+  att.value = "pixel active";
+  event.target.setAttributeNode(att);
+}
 
 // event listener for the resolution submit button
-const resolutionSubmit = document.querySelector("#resolutionSubmit");
+const resolutionSubmit = document.querySelector("#res-submit-btn");
 resolutionSubmit.addEventListener("click", () => {
   setResolution();
 });
+
+/**
+ * Checks the value of #resolution number input. Prompts if out of range
+ * otherwise clears #screen and re-populates the area with new .pixel elements
+ */
+function setResolution() {
+  const res = document.querySelector("#res-input").value;
+  if (res < 1) prompt("Resolution input value too low.");
+  else if (res > 100) prompt("Resolution input value too high.");
+  else {
+    createPixels(res, res);
+  }
+}
 
 // todo: reset button
 // todo: rainbow mode
